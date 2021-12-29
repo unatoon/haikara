702d833ac303ec9664db75fc89c134258e21ef85
unatoon <unatoon@users.noreply.github.com>
2021-12-29 06:25:18 +0000

    :lipstick: Adjust wave of the bottom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,18 @@ import * as React from "react";
 import { Parallax } from "@react-spring/parallax";
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
 import Hero from "@lekoarts/gatsby-theme-cara/src/components/hero";
-import About from "@lekoarts/gatsby-theme-cara/src/components/about";
-import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact";
+import ThemeAbout from "@lekoarts/gatsby-theme-cara/src/components/about";
 
 import Projects from "../components/Projects";
+import About from "../components/About";
 
 const Index = () => (
     <Layout>
         <Parallax pages={7}>
             <Hero offset={0} factor={1} />
             <Projects offset={1} factor={2} />
-            <About offset={3} factor={2} />
-            <Contact offset={5} factor={2} />
+            <ThemeAbout offset={3} factor={2} />
+            <About offset={5} factor={2} />
         </Parallax>
     </Layout>
 );
