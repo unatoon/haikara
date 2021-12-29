30650f5f2e5bbdc7e39c904ce05522223990e097
unatoon <unatoon@users.noreply.github.com>
2021-12-29 12:00:26 +0000

    Format gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://haikara.unatoon.ink",
-    title: "SNACK HAIKARA",
+    siteMetadata: {
+        siteUrl: "https://haikara.unatoon.ink",
+        title: "SNACK HAIKARA",
 
-    // For cara theme: https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara#additional-configuration
-    siteTitle: "SNACK HAIKARA",
-    siteTitleAlt: "SNACK HAIKARA",
-    siteHeadline: "エンジョイ スプラトゥーン",
-    siteDescription: "スナックハイカラのサイトです",
-    siteLanguage: "ja",
-    siteImage: "/images/banner-2.png",
-    author: "@unatoon",
-  },
-  plugins: ["gatsby-plugin-emotion",
-    {
-      resolve: "@lekoarts/gatsby-theme-cara",
-    }
-  ],
+        // For cara theme: https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara#additional-configuration
+        siteTitle: "SNACK HAIKARA",
+        siteTitleAlt: "SNACK HAIKARA",
+        siteHeadline: "エンジョイ スプラトゥーン",
+        siteDescription: "スナックハイカラのサイトです",
+        siteLanguage: "ja",
+        siteImage: "/images/banner-2.png",
+        author: "@unatoon",
+    },
+    plugins: [
+        "gatsby-plugin-emotion",
+        {
+            resolve: "@lekoarts/gatsby-theme-cara",
+        }
+    ],
 };
