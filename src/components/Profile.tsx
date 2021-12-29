02f6c7aec0550207dc6f432e12c5975a0dba1ad4
unatoon <unatoon@users.noreply.github.com>
2021-12-29 06:00:42 +0000

    :sparkles: Add About

---
+++ src/components/Profile.tsx
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
+import { Avatar, Card, Heading, Paragraph, Text } from "theme-ui";
+
+type ProfileProps = {
+    name: string;
+    nickname: string;
+    icon: string;
+    weapons: string[];
+};
+
+const Profile = ({ name, nickname, icon, weapons }: ProfileProps) => (
+    <div sx={{marginTop: [4, 4, 4, 5]}}>
+        <Card>
+            <div sx={{
+                display: "flex",
+                flexDirection: "row",
+                gap: [2, 2, 2, 3],
+            }}>
+                <Avatar
+                    src={icon}
+                    sx={{
+                        width: 32,
+                        height: 32,
+                    }}
+                />
+                <div sx={{flexGrow: 1}}>
+                    <Heading
+                        as="h3"
+                        sx={{
+                            fontSize: [2, 3, 4],
+                            mt: 2,
+                            color: "heading",
+                        }}
+                    >
+                        {name}
+                    </Heading>
+                    <Paragraph sx={{ textAlign: "right" }}>
+                        <Text
+                            sx={{
+                                fontStyle: "italic",
+                                fontWeight: "bold",
+                                textAlign: "right",
+                            }}
+                        >
+                            ― {nickname}
+                        </Text>
+
+                    </Paragraph>
+
+                </div>
+            </div>
+            <div>
+                <p>{weapons.join(" / ")}</p>
+            </div>
+        </Card>
+    </div>
+);
+
+export default Profile;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,11 @@ import Projects from "../components/Projects";
 
 const Index = () => (
     <Layout>
-        <Parallax pages={6}>
+        <Parallax pages={7}>
             <Hero offset={0} factor={1} />
             <Projects offset={1} factor={2} />
             <About offset={3} factor={2} />
-            <Contact offset={5} factor={1} />
+            <Contact offset={5} factor={2} />
         </Parallax>
     </Layout>
 );
